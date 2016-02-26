0941a97f65420953c51e3414b0c408441b8f6f9f
Leah <eahlay@gmail.com>
2016-02-26 11:23:35 -0500

    finish code reorganization

--- SemanticReader/src/DummyScorer.java
+++
@@ -1,9 +0,0 @@
-/**
- * An example implementation of the Scorer class
- */
-public class DummyScorer extends Scorer{
-
-    public double getScore(Entity entity, Document doc) {
-        return 1;
-    }
-}
