db9c4f5df52e7987085c04c758415f74adf38aa2
Shrimai <shrimai@Shrimais-MBP.wv.cc.cmu.edu>
2016-04-11 15:06:17 -0400

    Adding weights to score calculator

--- src/main/java/ScoreCalculator.java
+++ src/main/java/ScoreCalculator.java
@@ -6,15 +6,25 @@ import java.util.Set;
  * Created by shrimai on 4/1/16.
  */
 public class ScoreCalculator {
-    static ArrayList<Scorer> scorersList = new ArrayList<Scorer>();
+    static ArrayList<ScoreWeight<Scorer, Double>> scorersList = new ArrayList<ScoreWeight<Scorer, Double>>();
     private static List<Document> DocumentList;
+
+    class ScoreWeight<S, W> {
+        Scorer scorer;
+        double weight;
+
+        public ScoreWeight(Scorer scorer, double weight) {
+            this.scorer = scorer;
+            this.weight = weight;
+        }
+    }
 
     //create a list of all the scoring methods (represented by the abstract class Scorer), which will be applied to the data
     public ScoreCalculator(List<Document> docs){
         this.DocumentList = docs;
-        //scorersList.add(new WordcountScorer());
+        scorersList.add(new ScoreWeight<Scorer, Double>(new WordcountScorer(), 1.0));
         //scorersList.add(new SemaforScorer());
-        scorersList.add(new SentenceToVector(DocumentList));
+        scorersList.add(new ScoreWeight<Scorer, Double>(new SentenceToVector(DocumentList), 0.8));
     }
 
     //TODO create more complex/sophisticated scorers
@@ -23,31 +33,26 @@ public class ScoreCalculator {
     /**
      * Given a list of scores, combine them into a single score
      */
-    private static double combineScores(List<Double> scores) {
-        //scores are weighted evenly here
-        double sum = 0;
-        for (Double score : scores) {
-            sum += score;
-        }
-        return sum / scores.size();
-
+    private static double combineScores(Entity entity, double score, double weight) {
+        return (entity.getScore() + weight*score);
         //TODO train weights for scores?
     }
 
     public static void setScores() {
         int n = 1;
+        double score, current_weight;
 
         //iterate over the docs, finding a score for each entity
         for (Document doc : DocumentList) {
             System.out.println("Scoring doc " + n + "/" + DocumentList.size());
             n++;
             Set<Entity> entities = doc.getEntities();
-            for (Scorer scorer : scorersList) {
-                scorer.initializeScorer(doc);
-                List<Double> scores = new ArrayList();
+            for (ScoreWeight current_scorer : scorersList) {
+                current_scorer.scorer.initializeScorer(doc);
+                current_weight = current_scorer.weight;
                 for (Entity entity : entities) {
-                    scores.add(scorer.getScore(entity, doc));
-                    entity.setScore(combineScores(scores));
+                    score = current_scorer.scorer.getScore(entity, doc);
+                    entity.setScore(combineScores(entity, score, current_weight));
                 }
                 //the score is stored as part of the entity object
             }
